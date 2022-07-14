8f86b42101abbc182b9c843721724f23e7e8bcad
Xifre Font <xifre.font.97@gmail.com>
2022-07-14 22:27:54 +0200

    Push method finished and tested

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 const SkylabArray = require("./skylabArray/SkylabArray");
 
 const newobj = new SkylabArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+newobj.push("11");
+console.log(newobj);
--- js/skylabArray/SkylabArray.test.js
+++ js/skylabArray/SkylabArray.test.js
@@ -1,19 +1,5 @@
 const SkylabArray = require("./SkylabArray");
 
-describe("Given a function getLength", () => {
-  describe("When it's called and receives a value of 'Pedrito'", () => {
-    test("It should return 7", () => {
-      const testingObject = new SkylabArray();
-      const value = "Pedrito";
-      const expectedResult = 7;
-
-      const receivedResult = testingObject.getLength(value);
-
-      expect(receivedResult).toBe(expectedResult);
-    });
-  });
-});
-
 describe("Given a constructor function", () => {
   describe("When it's called and receives a value of '1, 2, 3, 4, 5, 6, 7, 8, 9, 10'", () => {
     test("It should return an object that emulates the form of an array with the same values", () => {
@@ -33,6 +19,47 @@ describe("Given a constructor function", () => {
       const receivedResult = new SkylabArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
       expect(expectedResult).toEqual(receivedResult);
+    });
+  });
+});
+
+describe("Given a function getLength", () => {
+  describe("When it's called and receives a value of 'Pedrito'", () => {
+    test("It should return 7", () => {
+      const testingObject = new SkylabArray();
+      const value = "Pedrito";
+
+      const expectedResult = 7;
+
+      const receivedResult = testingObject.getLength(value);
+
+      expect(expectedResult).toBe(receivedResult);
+    });
+  });
+});
+
+describe("Given a function push()", () => {
+  describe("When it's called an receives a value of 11", () => {
+    test("It should return the same object adding a last value with 11", () => {
+      const testingObject = new SkylabArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+      const value = 11;
+      const expectedResult = {
+        0: 1,
+        1: 2,
+        2: 3,
+        3: 4,
+        4: 5,
+        5: 6,
+        6: 7,
+        7: 8,
+        8: 9,
+        9: 10,
+        10: 11,
+      };
+
+      const receivedResult = testingObject.push(value);
+
+      expect(receivedResult).toEqual(expectedResult);
     });
   });
 });
