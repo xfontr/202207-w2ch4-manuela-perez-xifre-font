873199a5233b6fc1dc8f502b91fe21668242d9a2
Xifre Font <xifre.font.97@gmail.com>
2022-07-14 19:23:15 +0200

    Added length feature and fully tested it

--- js/skylabArray/SkylabArray.js
+++ js/skylabArray/SkylabArray.js
@@ -1,3 +1,20 @@
-class SkylabArray {}
+class SkylabArray {
+  value;
+  length = this.getLength(this.value);
 
-export default SkylabArray;
+  constructor(value) {
+    this.value = value;
+  }
+
+  getLength(value) {
+    let i = 1;
+    do {
+      i += 1;
+    } while (value[i] !== undefined);
+
+    this.length = i;
+    return i;
+  }
+}
+
+module.exports = SkylabArray;
